0a2eea09c7a1a2397685485e1ccb5c44726cd2ea
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-06-02 20:40:18 -0300

    feat: repository extends jpa

---
+++ src/main/java/com/gurodriguesdev/products/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.gurodriguesdev.products.repositories;
+
+import com.gurodriguesdev.products.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
